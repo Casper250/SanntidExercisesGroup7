f74ef003d43001145051078ff84c03631bbd2fda
Eskil <eskilaf@stud.ntnu.no>
2026-01-19 13:14:36 +0100

    tcp i go

---
+++ Exercise2-go/tcp.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go tcp_send("Hello from Eskil sin laptop", &wg)
+	go tcp_send("Hello from another tcp_send() function", &wg)
+	wg.Wait()
+}
+
+func tcp_send(message string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var terminatorChar = "\x00"
+	var counter = 0
+	data := []byte(message + terminatorChar)
+
+	// TCP socket
+	// Portnummeret kom på VsCode med den lokale severen
+	//var port_delimMessage = "33546"
+	var port_fixedMessageSize = "34933"
+	var ip_serverSide = "172.26.161.38"
+	var address = ip_serverSide + ":" + port_fixedMessageSize
+
+	// Make a tcp connection
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatalln(conn, "Connection failed")
+		return
+	}
+	defer conn.Close()
+
+	for counter < 10 {
+
+		// Send a message
+		conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
+		_, err = conn.Write(data)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// Read a response
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Println(err)
+		}
+
+		// Print response
+		fmt.Println("DATA FROM SERVER: ", string(buf[:n]))
+
+		time.Sleep(333 * time.Millisecond)
+		counter++
+	}
+
+}
